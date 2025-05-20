bd948f27dfe60dd32454b5f98028ec73f6451dc8
jp-soaresr <soaresr0105@gmail.com>
2025-05-19 22:13:34 -0300

    fim

--- src/Componentes/Gallery.js
+++ src/Componentes/Gallery.js
@@ -2,7 +2,6 @@ function Profile() {
     return (
         <img 
             src="https://i.imgur.com/MK3eW3As.jpg"
-            alt="Katherine Johnson"
             alt="Katherine Johnson" // Katherine Johnson
         />
     );
@@ -11,7 +10,6 @@ function Profile() {
 export default function Gallery() {
     return (
         <section>
-            <h1>Amazing Scientist</h1>
             <h1>Cientistas Incríveis</h1>
             <Profile />
             <Profile />
